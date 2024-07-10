0b843e79aa22b5c7ae5ba8ff560f85e495d59f68
Abdelrahman Salem <67508048+abdelrahman-20@users.noreply.github.com>
2024-07-11 01:41:33 +0300

    Update BaseModel Class

--- models/base_model.py
+++ models/base_model.py
@@ -2,23 +2,28 @@
 """The Base Model of The Project"""
 
 import uuid
-import datetime
+from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
     """The BaseModel Class For AirBnB Project."""
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        """The Constructor Function of The Class."""
+        if kwargs:
+            del kwargs["__class__"]
+            for k, v in kwargs.items():
+                if k in ["created_at", "updated_at"]:
+                    setattr(self, k, datetime.fromisoformat(v))
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
-
-    def __str__(self):
-        cls_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
@@ -26,3 +31,7 @@ class BaseModel():
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
+
+    def __str__(self):
+        cls_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
