e0837593932c621da20913b53a980da26a0a6fd6
Abdelrahman Salem <67508048+abdelrahman-20@users.noreply.github.com>
2024-07-11 20:31:43 +0300

    Initialize FileStorage Class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""The File Storage Model of The Project"""
+
+import os
+import json
+
+
+class FileStorage:
+    """A Class To Handle Storing Data."""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Function To Return Objects Dictionary"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set The Object Key and Value in __objects"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects To The JSON File"""
+        with open(FileStorage.__file_path, "w") as fp:
+            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, fp)
+
+    def reload(self):
+        """Deserializes __objects From The JSON File"""
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as fp:
+                FileStorage.__objects = json.load(fp)
