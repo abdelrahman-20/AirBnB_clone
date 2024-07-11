ade477292d9475306d73696add7bc547d4df4763
Abdelrahman Salem <67508048+abdelrahman-20@users.noreply.github.com>
2024-07-11 20:37:31 +0300

    Initialize FileStorage Class

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -21,9 +22,11 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
